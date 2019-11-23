d7263edba012855672d83d5824a32f4afe9c108d
tonorzenko <tonorzenko@gmail.com>
2019-11-24 00:31:47 +0200

    Upload assets and functions.php

---
+++ functions.php
@@ -0,0 +1,26 @@
+<?php
+// Add child parent and child theme styles
+add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
+
+function enqueue_styles() {
+   wp_enqueue_style( 'parent-styles', get_template_directory_uri().'/style.css' );
+   wp_enqueue_style( 'child-styles', get_stylesheet_directory_uri().'/assets/css/frontend.css' );
+}
+
+// Add css for gutenberg blocks inside backend
+add_action( 'enqueue_block_editor_assets', 'twentytwenty_child_block_editor_styles', 99 );
+
+function twentytwenty_child_block_editor_styles() {
+	wp_enqueue_style( 'twentytwenty-child-block-editor-styles', get_stylesheet_directory_uri().'/assets/css/gutenberg.css' );
+}
+
+// Remove state for Woocommerce calculator on cart page
+add_filter( 'woocommerce_shipping_calculator_enable_state', '__return_false' );
+
+// Remove the sorting dropdown from Woocommerce
+remove_action( 'woocommerce_before_shop_loop' , 'woocommerce_catalog_ordering', 30 );
+
+// Remove the result count from WooCommerce
+remove_action( 'woocommerce_before_shop_loop' , 'woocommerce_result_count', 20 );
+
+?>
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Child theme for new Twenty Twenty Wordpress theme
+
+This theme make some changes for Woocommerce plugin. Now it is looks better.
+
+## Installation
+
+1. In your admin panel, go to Appearance -> Themes and click the 'Add New' button.
+2. Type in Twenty Twenty in the search form and install id but NOT activate.
+3. Download that children theme from repository, and install it by uploading as file.
+4. Activate Twenty Twenty child Theme and enjoy it!
